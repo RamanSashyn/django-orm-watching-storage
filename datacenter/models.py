b69a927baa0e2d3254f1c141ff48447e61974ea7
Raman <149716575+RamanSashyn@users.noreply.github.com>
2025-01-16 18:05:59 +0100

    Добавление функций get_entered_at_formatted, format_duration, get_duration

--- datacenter/models.py
+++ datacenter/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils.timezone import localtime
+from datetime import timedelta
 
 
 class Passcard(models.Model):
@@ -28,3 +30,28 @@ class Visit(models.Model):
                 if self.leaved_at else 'not leaved'
             )
         )
+
+    def get_duration(self):
+        if self.leaved_at:
+            return self.leaved_at - self.entered_at
+        return localtime() - self.entered_at
+
+    def format_duration(self):
+        duration = self.get_duration()
+        total_seconds = int(duration.total_seconds())
+        hours = total_seconds // 3600
+        minutes = (total_seconds % 3600) // 60
+        return f'{hours:02}:{minutes:02}'
+
+    def get_entered_at_formatted(self):
+        entered_at_local = localtime(self.entered_at)
+        MONTHS = [
+            'января', 'февраля', 'марта', 'апреля', 'мая', 'июня',
+            'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'
+        ]
+        return f"{entered_at_local.day} {MONTHS[entered_at_local.month - 1]} {entered_at_local.year} г. " \
+               f"{entered_at_local.hour:02}:{entered_at_local.minute:02}"
+
+
+
+
