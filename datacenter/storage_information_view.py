4fbb658868ec7fb7f56c4f3f02beb697bb66586d
Raman <149716575+RamanSashyn@users.noreply.github.com>
2025-01-16 18:07:06 +0100

    Отображение кто находиться в хранилище

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,19 +1,20 @@
-from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
 
 
 def storage_information_view(request):
-    # Программируем здесь
+    active_visits = Visit.objects.filter(leaved_at__isnull=True)
 
     non_closed_visits = [
         {
-            'who_entered': 'Richard Shaw',
-            'entered_at': '11-04-2018 25:34',
-            'duration': '25:03',
+            'who_entered': visit.passcard.owner_name,
+            'entered_at': visit.get_entered_at_formatted(),
+            'duration': visit.format_duration(),
         }
+        for visit in active_visits
     ]
+
     context = {
-        'non_closed_visits': non_closed_visits,  # не закрытые посещения
+        'non_closed_visits': non_closed_visits,
     }
     return render(request, 'storage_information.html', context)
