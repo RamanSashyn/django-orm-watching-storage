a68fc159a293665f04f773432147af8f642a4980
Raman <149716575+RamanSashyn@users.noreply.github.com>
2025-01-27 14:35:59 +0100

    Уменьшил требуемые настройки; Высушил код.

--- project/settings.py
+++ project/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 
 DEBUG = env.bool("DEBUG", default=True)
 
-ALLOWED_HOSTS = env.str('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost', '127.0.0.1'])
 
 INSTALLED_APPS = ['datacenter']
 ROOT_URLCONF = 'project.urls'
